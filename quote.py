8d7d523a047cbcd46ed55a7da3ba1ec5140c36ae
EFFLUX <bhowmickmrinank@gmail.com>
2021-09-23 16:53:36 +0530

    fixed some error

--- quote.py
+++ quote.py
@@ -1,7 +1,7 @@
 import random,os
 #from flask import jsonify
 def life_quote():
-    fin=open('quotes_files\\lifequotes.txt','r')
+    fin=open('quotes_files//lifequotes.txt','r')
     for count, line in enumerate(fin):
         pass
     fin.seek(0)
@@ -15,7 +15,7 @@ def life_quote():
     return result
 
 def business_quote():
-    fin=open('quotes_files\\business_quotes.txt','r')
+    fin=open('quotes_files//business_quotes.txt','r')
     for count, line in enumerate(fin):
         pass
     fin.seek(0)
@@ -29,7 +29,7 @@ def business_quote():
     return result
 
 def friendship_quote():
-    fin=open('quotes_files\\friendship_quote.txt','r')
+    fin=open('quotes_files//friendship_quote.txt','r')
     for count, line in enumerate(fin):
         pass
     fin.seek(0)
@@ -43,7 +43,7 @@ def friendship_quote():
     return result
 
 def random_quote():
-    fin=open('quotes_files\\quote_random.txt','r')
+    fin=open('quotes_files//quote_random.txt','r')
     for count, line in enumerate(fin):
         pass
     fin.seek(0)
